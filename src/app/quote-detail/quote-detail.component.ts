fef62b5def044c5d2ace63798e09429f197ea418
michael otieno <michael.otieno@student.moringaschool.com>
2021-08-21 10:58:09 +0300

    add delete quote event to the quote detail component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote'
 
 @Component({
@@ -9,10 +9,15 @@ import { Quote } from '../quote'
 export class QuoteDetailComponent implements OnInit {
 
   @Input() quote!: Quote;
+  @Output() isComplete=new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
