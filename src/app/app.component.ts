1066beb9cff63211bbce8337b53bde8de9423490
michael otieno <michael.otieno@student.moringaschool.com>
2021-08-21 08:43:18 +0300

    add files create component quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:Quote[] = [
+    {authorname: 'Elon Musk', name:'John Kiarie'},
+    {authorname: 'Enzo Ferrari', name: 'Peris Wanyama'},
+    {authorname: 'Lewis Hamilton', name:'Joseph Wafula'}
+    
+  ];
+
+ 
 }
