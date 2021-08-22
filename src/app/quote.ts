80ccb41270ac94d4ac8dd04d9d0e5bb2da3e4af9
michael otieno <michael.otieno@student.moringaschool.com>
2021-08-22 09:03:56 +0300

    add id

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,8 +4,8 @@ export class Quote {
     name?: string;
     description?: string
     */
-   showDescription : boolean;
-   constructor(public authorname: string, public name: string, public description: string, public completeDate: Date){
+   public showDescription : boolean;//
+   constructor(public id:number, public authorname: string, public name: string, public description: string, public completeDate: Date){
        this.showDescription = false;
    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
-  quotes:Quote[] = [
-    new Quote('Elon Musk','John Kiarie','Sel-driving cars are the natural extension of active safety and obviously something we should do.', new Date(2020,1,9)),
-    new Quote('Enzo Ferrari', 'Peris Wanyama', "The fact is I don't just drive to get from A to B. I enjoy feeling the cars reactions, becoming part of it.", new Date(2021,5,1)),
-    new Quote('Lewis Hamilton', 'Joseph Wafula', "The way I drive, the way I handle a car, is an expression of my inner feelings.",new Date(2021,5,20))
+  quotes:Quote[] = [//
+    new Quote(1,'Elon Musk','John Kiarie','Sel-driving cars are the natural extension of active safety and obviously something we should do.', new Date(2020,1,9)),
+    new Quote(2,'Enzo Ferrari', 'Peris Wanyama', "The fact is I don't just drive to get from A to B. I enjoy feeling the cars reactions, becoming part of it.", new Date(2021,5,1)),
+    new Quote(3,'Lewis Hamilton', 'Joseph Wafula', "The way I drive, the way I handle a car, is an expression of my inner feelings.",new Date(2021,5,20))
     
   ];
 
